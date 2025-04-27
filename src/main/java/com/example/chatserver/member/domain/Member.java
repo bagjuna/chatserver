11d0bebc2307c6e1c5198b13991a479aae744dfd
bagjuna <juna.park124@gmail.com>
2025-04-27 23:39:18 +0900

    first commit

---
+++ src/main/java/com/example/chatserver/member/domain/Member.java
@@ -0,0 +1,29 @@
+package com.example.chatserver.member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Role role = Role.USER;
+
+
+}
